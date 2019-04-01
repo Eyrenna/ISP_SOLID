2a5222aada6bd20b8ff67501f79f5ec97a0fbe46
Claudia Gázquez Alessio <claudia365x@gmail.com>
2019-04-02 01:39:25 +0200

    Creación interfaz extensile

--- src/main/java/org/lasencinas/ProcesadorTexto.java
+++ src/main/java/org/lasencinas/ProcesadorTexto.java
@@ -4,15 +4,19 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors; /*java8*/
 
-public class ProcesadorTexto {
+public class ProcesadorTexto implements extensible {
 
 
 	private List<String> texto = new ArrayList<>();
-	
-	public void nueva (String palabra) {
+
+
+	@Override
+	public void nueva(String palabra) {
 		texto.add(palabra);
 	}
-	
+
+
+    @Override
 	public String texto () {
 		return texto.stream().collect(Collectors.joining(" "));
 	}
---
+++ src/main/java/org/lasencinas/extensible.java
@@ -0,0 +1,10 @@
+package org.lasencinas;
+
+public interface extensible {
+
+    void nueva(String palabra);
+
+    String texto();
+
+
+}
